6a754e044745301430827dab2cfae211a17cdd8e
Kaiky Tupinambá <kaikymoura972@gmail.com>
2025-04-25 14:33:34 -0300

    Firt commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,73 @@
+"use client";
 import Image from "next/image";
+import Link from "next/link";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className={styles.container}>
+      <div className={`fixed z-120 ${styles.header}`}>
+        <h2 className="font-bold text-xl text-center lg:text-left">&lt;&gt; Kaiky Tupinambá &lt;/&gt;</h2>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="grid grid-cols-1 lg:grid-cols-3 items-start justify-items-center lg:justify-items-stretch gap-16 px-6 pt-32 pb-20 sm:px-20">
+
+        <div className="flex flex-col items-center text-center gap-6 lg:items-start lg:text-left">
+          <Image
+            className="rounded-full border-4 border-gray-200 shadow-lg hover:scale-105 transition-transform duration-300"
+            src="/profile.png"
+            alt="Picture of the author"
+            width={150}
+            height={150}
+          />
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-bold">Hi, I&apos;m Kaiky</h1>
+            <h2 className="text-2xl font-semibold text-gray-600">Fullstack Developer</h2>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+
+        <div className="flex flex-col gap-4 text-center lg:text-left">
+          <h3 className="text-2xl font-bold">About me</h3>
+          <p className="text-gray-700 hover:text-white">
+            I&apos;m a Fullstack developer with a strong focus on front-end technologies. Passionate about building beautiful UIs and delivering smooth user experiences.
+          </p>
+          <p className="text-gray-700 hover:text-white">
+          And also build APIs and backend services, focusing on creating scalable, efficient, and secure solutions for modern web applications.
+          </p>
+        </div>
+
+        <div className="flex flex-col gap-4 items-center w-full">
+          <iframe
+            className="w-full rounded shadow-md"
+            src="https://github-readme-stats.vercel.app/api?username=kaikyMoura&show_icons=true&theme=dracula"
+            height="150"
+            title="GitHub stats"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+          <iframe
+            className="w-full rounded shadow-md"
+            src="https://github-readme-stats.vercel.app/api/top-langs/?username=kaikyMoura&layout=compact&theme=dracula"
+            height="150"
+            title="GitHub top languages"
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+        <div className="lg:col-span-2 flex justify-center lg:justify-end gap-4 flex-wrap pt-4">
+          <Link
+            className="flex items-center gap-2 border border-gray-300 hover:bg-gray-100 hover:text-black transition rounded px-4 py-2"
+            href="https://github.com/kaikyMoura"
+          >
+            <FaGithub fontSize={20} />
+            <span>GitHub</span>
+          </Link>
+          <Link
+            className="flex items-center gap-2 border border-gray-300 hover:bg-gray-100 hover:text-black transition rounded px-4 py-2"
+            href="https://www.linkedin.com/in/kaiky-de-moura-tupinamba/"
+          >
+            <FaLinkedin fontSize={20} />
+            <span>LinkedIn</span>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
